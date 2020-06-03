ed8067e47c890db9baa70b1aa625a479ebbf3b58
Ryo Manzoku <manzoku.ryo@gmail.com>
2020-06-03 07:12:07 +0000

    sig works!

--- lbd.go
+++ lbd.go
@@ -1,20 +1,32 @@
 package lbd
 
 import (
+	"crypto/hmac"
+	"crypto/sha512"
+	"encoding/base64"
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
+	"math/rand"
 	"net/http"
-	"strings"
+	"time"
 )
 
 const (
 	CashewBaseURL = "https://test-api-blockchain.line.me"
 )
 
+var (
+	rsLetterIdxBits       = 6
+	rsLetterIdxMask int64 = 1<<rsLetterIdxBits - 1
+	rsLetterIdxMax        = 63 / rsLetterIdxBits
+	randSrc               = rand.NewSource(time.Now().UnixNano())
+)
+
 type LBD struct {
-	baseURL string
-	APIKey  string
+	baseURL   string
+	apiKey    string
+	apiSecret string
 }
 
 type Response struct {
@@ -24,34 +36,59 @@ type Response struct {
 	ResponseData  interface{} `json:"responseData"`
 }
 
-func NewLBD(apiKey string) (*LBD, error) {
+func NewLBD(apiKey string, secret string) (*LBD, error) {
 	l := &LBD{
-		baseURL: CashewBaseURL,
-		APIKey:  apiKey,
+		baseURL:   CashewBaseURL,
+		apiKey:    apiKey,
+		apiSecret: secret,
 	}
 	return l, nil
 }
 
+func (l LBD) Sign(nonce string, timestampMsec int64, method, path, query string) string {
+	return sign(l.apiSecret, nonce, timestampMsec, method, path, query)
+}
+
+func (l LBD) RetrieveServiceInformation(serviceId string) (string, error) {
+	path := "/v1/services/" + serviceId
+
+	ret, err := l.get(path, "", true)
+	if err != nil {
+		return "", err
+	}
+
+	fmt.Println(ret)
+	return "ok", nil
+}
+
 func (l LBD) RetrieveServerTime() (int64, error) {
 	path := "/v1/time"
-	ret, err := l.get(path)
+	ret, err := l.get(path, "", false)
 	if err != nil {
 		return 0, err
 	}
 	return ret.ResponseTime, nil
 }
 
-func (l LBD) get(path string) (*Response, error) {
-	if !strings.HasPrefix(path, "/") {
-		path = "/" + path
-	}
+func (l LBD) get(path, query string, sign bool) (*Response, error) {
 	url := l.baseURL + path
 
 	fmt.Println(url)
 
 	client := new(http.Client)
 	req, err := http.NewRequest("GET", url, nil)
-	req.Header.Add("service-api-key", l.APIKey)
+	req.Header.Add("service-api-key", l.apiKey)
+
+	if sign {
+		timeMsec := NowMsec()
+		nonce := GenerateNonce(timeMsec)
+		sig := l.Sign(nonce, timeMsec, "GET", path, query)
+
+		fmt.Println(nonce, fmt.Sprint(timeMsec), sig)
+		req.Header.Add("nonce", nonce)
+		req.Header.Add("timestamp", fmt.Sprint(timeMsec))
+		req.Header.Add("signature", sig)
+	}
 
 	resp, err := client.Do(req)
 	if err != nil {
@@ -75,4 +112,48 @@ func (l LBD) get(path string) (*Response, error) {
 	}
 
 	return ret, nil
+}
+
+func NowMsec() int64 {
+	return time.Now().UnixNano() / 1000000
+}
+
+func GenerateNonce(timestampMsec int64) string {
+	// TODO The same nonce can’t be reused per service-api-key within 20 seconds.
+	// An error is returned when the nonce of the successful request is reused within 20 seconds.
+	return randString(8)
+}
+
+func sign(secret, nonce string, timestampMsec int64, method, path string, query string) string {
+	var msg string
+	if query == "" {
+		msg = fmt.Sprintf("%s%d%s%s", nonce, timestampMsec, method, path)
+	} else {
+		msg = fmt.Sprintf("%s%d%s%s?%s", nonce, timestampMsec, method, path, query)
+	}
+
+	mac := hmac.New(sha512.New, []byte(secret))
+	mac.Write([]byte(msg))
+	sig := mac.Sum(nil)
+
+	return base64.StdEncoding.EncodeToString(sig)
+}
+
+func randString(l int) string {
+	rsLetters := "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+	b := make([]byte, l)
+	cache, remain := randSrc.Int63(), rsLetterIdxMax
+	for i := l - 1; i >= 0; {
+		if remain == 0 {
+			cache, remain = randSrc.Int63(), rsLetterIdxMax
+		}
+		idx := int(cache & rsLetterIdxMask)
+		if idx < len(rsLetters) {
+			b[i] = rsLetters[idx]
+			i--
+		}
+		cache >>= rsLetterIdxBits
+		remain--
+	}
+	return string(b)
 }
--- lbd_test.go
+++ lbd_test.go
@@ -9,8 +9,17 @@ import (
 )
 
 var (
-	l = &LBD{}
+	l         = &LBD{}
+	serviceID = os.Getenv("SERVICE_ID")
 )
+
+func TestRetrieveServiceInformation(t *testing.T) {
+	is := initializeTest(t)
+	ret, err := l.RetrieveServiceInformation(serviceID)
+	is.Nil(err)
+
+	fmt.Println(ret)
+}
 
 func TestRetrieveServerTime(t *testing.T) {
 	is := initializeTest(t)
@@ -19,11 +28,24 @@ func TestRetrieveServerTime(t *testing.T) {
 	fmt.Println("Server time:", tm)
 }
 
+func TestSign(t *testing.T) {
+	is := initializeTest(t)
+	secret := "9256bf8a-2b86-42fe-b3e0-d3079d0141fe"
+	nonce := "Bp0IqgXE"
+	timestamp := int64(1581850266351)
+	method := "GET"
+	path := "/v1/wallets"
+	sig := sign(secret, nonce, timestamp, method, path, "")
+
+	expected := "2LtyRNI16y/5/RdoTB65sfLkO0OSJ4pCuz2+ar0npkRbk1/dqq1fbt1FZo7fueQl1umKWWlBGu/53KD2cptcCA=="
+	is.Equal(sig, expected)
+}
+
 func initializeTest(t *testing.T) is.I {
 	is := is.New(t)
 	var err error
 
-	l, err = NewLBD(os.Getenv("API_KEY"))
+	l, err = NewLBD(os.Getenv("API_KEY"), os.Getenv("API_SECRET"))
 	is.Nil(err)
 	return is
 }
