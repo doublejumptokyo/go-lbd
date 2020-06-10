3655e79f901aed7a8ca641484bbc314a4767e46d
Ryo Manzoku <manzoku.ryo@gmail.com>
2020-06-10 11:30:36 +0000

    Tx hash from env

--- transactions_test.go
+++ transactions_test.go
@@ -1,11 +1,16 @@
 package lbd
 
-import "testing"
+import (
+	"os"
+	"testing"
+)
 
 func TestRetrieveTransactionInformation(t *testing.T) {
 	is := initializeTest(t)
-	txHash := "E848200D92C1AD9D12B6A5A044090D32E95B13D5A7668D37D5583E5D53A7EC2F"
-	ret, err := l.RetrieveTransactionInformation(txHash)
+	if os.Getenv("TX_HASH") == "" {
+		t.Skip()
+	}
+	ret, err := l.RetrieveTransactionInformation(os.Getenv("TX_HASH"))
 	is.Nil(err)
 	t.Log(ret)
 }
