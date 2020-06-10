597ce305a7957473ed94d0c8ce76694fc827e764
shunfuruya <shun.furuya1987@gmail.com>
2020-06-10 16:24:19 +0900

    fix

--- memos_test.go
+++ memos_test.go
@@ -6,6 +6,14 @@ import (
 )
 
 
+func TestSaveTheText(t *testing.T) {
+	onlyTxMode(t)
+	is := initializeTest(t)
+	ret, err := l.SaveTheText()
+	is.Nil(err)
+	t.Log(ret)
+}
+
 func TestRetrieveTheText(t *testing.T) {
 	is := initializeTest(t)
 	txHash := "E848200D92C1AD9D12B6A5A044090D32E95B13D5A7668D37D5583E5D53A7EC2F"
