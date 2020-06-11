a57e876a99818062021d17e6088c3efd3cce8db7
Ryo Manzoku <manzoku.ryo@gmail.com>
2020-06-11 02:44:14 +0000

    testing msg

--- memos_test.go
+++ memos_test.go
@@ -3,23 +3,31 @@ package lbd
 import (
 	"os"
 	"testing"
+	"time"
 )
 
+var (
+	memoMsg    = "てすとだよー"
+	memoTxHash = os.Getenv("MEMOS_TX_HASH")
+)
 
 func TestSaveText(t *testing.T) {
 	onlyTxMode(t)
 	is := initializeTest(t)
-	ret, err := l.SaveText("てすとだよー",owner)
+	ret, err := l.SaveText("てすとだよー", owner)
 	is.Nil(err)
 	t.Log(ret)
 }
 
 func TestRetrieveTheText(t *testing.T) {
 	is := initializeTest(t)
-	if os.Getenv("MEMOS_TX_HASH") == "" {
+	if memoTxHash == "" {
 		t.Skip()
 	}
-	ret, err := l.RetrieveTheText(os.Getenv("MEMOS_TX_HASH"))
+	time.Sleep(2 * time.Second)
+	ret, err := l.RetrieveTheText(memoTxHash)
+	is.Equal(memoMsg, ret.Memo)
+
 	is.Nil(err)
 	t.Log(ret)
 }
