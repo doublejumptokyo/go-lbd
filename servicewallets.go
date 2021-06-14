a17668444d4be88a51e5256b1a62a7f8b7359d29
Ryo Manzoku <manzoku.ryo@gmail.com>
2021-06-15 03:38:16 +0900

    fix order

--- servicewallets.go
+++ servicewallets.go
@@ -81,12 +81,12 @@ type TransferNonFungibleServiceWalletRequest struct {
 func (r TransferNonFungibleServiceWalletRequest) Encode() string {
 	base := r.Request.Encode()
 	if r.ToUserId != "" {
-		return fmt.Sprintf("%s?toUserId=%s&walletSecret=%s", base, r.WalletSecret, r.ToUserId)
+		return fmt.Sprintf("%s?toUserId=%s&walletSecret=%s", base, r.ToUserId, r.WalletSecret)
 	}
 	return fmt.Sprintf("%s?toAddress=%s&walletSecret=%s", base, r.ToAddress, r.WalletSecret)
 }
 
-func (l *LBD) TransferNonFungibleServiceWallet(contractId, walletAddress, walletSecret, to, tokenType, tokenIndex string) (*Transaction, error) {
+func (l *LBD) TransferNonFungibleServiceWallet(walletAddress, walletSecret, contractId, to, tokenType, tokenIndex string) (*Transaction, error) {
 	path := fmt.Sprintf("/v1/wallets/%s/item-tokens/%s/non-fungibles/%s/%s/transfer", walletAddress, contractId, tokenType, tokenIndex)
 	r := &TransferNonFungibleServiceWalletRequest{
 		Request:      NewPostRequest(path),
@@ -104,4 +104,63 @@ func (l *LBD) TransferNonFungibleServiceWallet(contractId, walletAddress, wallet
 		return nil, err
 	}
 	return UnmarshalTransaction(resp.ResponseData)
+}
+
+func (l *LBD) RetrieveBalanceOfAllNonFungiblesServiceWallet(walletAddress, contractId string) ([]*NonFungible, error) {
+	path := fmt.Sprintf("/v1/wallets/%s/item-tokens/%s/non-fungibles", walletAddress, contractId)
+
+	all := []*NonFungible{}
+	page := 1
+	for {
+		r := NewGetRequest(path)
+		r.pager.Page = page
+		r.pager.OrderBy = "asc"
+		resp, err := l.Do(r, true)
+		if err != nil {
+			return nil, err
+		}
+		ret := []*NonFungible{}
+		err = json.Unmarshal(resp.ResponseData, &ret)
+		if err != nil {
+			return nil, err
+		}
+		if len(ret) == 0 {
+			break
+		}
+
+		all = append(all, ret...)
+		page++
+	}
+	return all, nil
+}
+
+func (l LBD) RetrieveBalanceOfSpecificTypeOfNonFungiblesServiceWallet(walletAddress, contractId, tokenType string) ([]*NonFungibleToken, error) {
+	path := fmt.Sprintf("/v1/wallets/%s/item-tokens/%s/non-fungibles/%s", walletAddress, contractId, tokenType)
+
+	all := []*NonFungibleToken{}
+	page := 1
+	for {
+		r := NewGetRequest(path)
+		r.pager.Page = page
+		r.pager.OrderBy = "asc"
+		resp, err := l.Do(r, true)
+		if err != nil {
+			return nil, err
+		}
+		ret := []*NonFungibleToken{}
+		err = json.Unmarshal(resp.ResponseData, &ret)
+		if err != nil {
+			return nil, err
+		}
+		if len(ret) == 0 {
+			break
+		}
+		all = append(all, ret...)
+		page++
+	}
+
+	for _, t := range all {
+		t.TokenType = tokenType
+	}
+	return all, nil
 }
