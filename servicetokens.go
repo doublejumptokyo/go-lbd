d2f381e436123621d45392183af27e2a95ebab12
kazuhiro516 <k.kurokawa11@gmail.com>
2022-01-18 21:00:40 +0900

    add servicetokens api function

--- servicetokens.go
+++ servicetokens.go
@@ -81,3 +81,82 @@ func (l *LBD) MintServiceToken(contractId string, to string, amount *big.Int) (*
 	}
 	return UnmarshalTransaction(resp.ResponseData)
 }
+
+type UpdateServiceTokenInformationRequest struct {
+	*Request
+	OwnerAddress string `json:"ownerAddress"`
+	OwnerSecret  string `json:"ownerSecret"`
+	Name         string `json:"name"`
+	Meta         string `json:"meta"`
+}
+
+func (l *LBD) UpdateServiceTokenInformation(contractId, name, meta string) (*Transaction, error) {
+	path := fmt.Sprintf("/v1/service-tokens/%s", contractId)
+
+	r := UpdateServiceTokenInformationRequest{
+		Request:      NewPutRequest(path),
+		OwnerAddress: l.Owner.Address,
+		OwnerSecret:  l.Owner.Secret,
+		Name:         name,
+		Meta:         meta,
+	}
+
+	resp, err := l.Do(r, true)
+	if err != nil {
+		return nil, err
+	}
+
+	return UnmarshalTransaction(resp.ResponseData)
+}
+
+type BurnServiceTokenRequest struct {
+	*Request
+	OwnerAddress string `json:"ownerAddress"`
+	OwnerSecret  string `json:"ownerSecret"`
+	Amount       string `json:"amount"`
+	FromUserId   string `json:"fromUserId,omitempty"`
+	FromAddress  string `json:"fromAddress,omitempty"`
+}
+
+func (l *LBD) BurnServiceToken(contractId, from string, amount *big.Int) (*Transaction, error) {
+	path := fmt.Sprintf("/v1/service-tokens/%s/burn-from", contractId)
+
+	r := BurnServiceTokenRequest{
+		Request:      NewPostRequest(path),
+		OwnerAddress: l.Owner.Address,
+		OwnerSecret:  l.Owner.Address,
+		Amount:       amount.String(),
+	}
+
+	if l.IsAddress(from) {
+		r.FromAddress = from
+	} else {
+		r.FromUserId = from
+	}
+
+	resp, err := l.Do(r, true)
+	if err != nil {
+		return nil, err
+	}
+	return UnmarshalTransaction(resp.ResponseData)
+}
+
+type ServiceTokenHolders struct {
+	Address string `json:"address"`
+	UserID  string `json:"userId"`
+	Amount  string `json:"amount"`
+}
+
+func (l *LBD) ListAllServiceTokenHolders(contentId string) ([]*ServiceTokenHolders, error) {
+	path := fmt.Sprintf("/v1/service-tokens/%s/holders", contentId)
+
+	r := NewGetRequest(path)
+
+	resp, err := l.Do(r, true)
+	if err != nil {
+		return nil, err
+	}
+
+	ret := []*ServiceTokenHolders{}
+	return ret, json.Unmarshal(resp.ResponseData, &ret)
+}
--- servicetokens_test.go
+++ servicetokens_test.go
@@ -8,6 +8,8 @@ import (
 )
 
 var serviceTokenContractId = "d5e19f47"
+var name = "test_user"
+var meta = "Test Api Function"
 
 func TestListAllServiceTokens(t *testing.T) {
 	ret, err := l.ListAllServiceTokens()
@@ -26,4 +28,27 @@ func TestMintServiceToken(t *testing.T) {
 	ret, err := l.MintServiceToken(serviceTokenContractId, toAddress, big.NewInt(1000))
 	assert.Nil(t, err)
 	t.Log(ret)
+}
+
+func TestUpdateServiceTokenInformation(t *testing.T) {
+	onlyTxMode(t)
+	ret, err := l.UpdateServiceTokenInformation(serviceTokenContractId, name, meta)
+	if err != nil {
+		t.Fatal(err)
+	}
+	t.Log(ret)
+}
+
+func TestBurnServiceToken(t *testing.T) {
+	onlyTxMode(t)
+	ret, err := l.BurnServiceToken(serviceTokenContractId, toAddress, big.NewInt(1000))
+	assert.Nil(t, err)
+	t.Log(ret)
+}
+
+func TestListAllServiceTokenHolders(t *testing.T) {
+	onlyTxMode(t)
+	ret, err := l.ListAllServiceTokenHolders(serviceTokenContractId)
+	assert.Nil(t, err)
+	t.Log(ret[0])
 }
