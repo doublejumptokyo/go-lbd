bfd7ff3ef40f7c27f316ba643fa1ed4a5e90f16d
kazuhiro516 <k.kurokawa11@gmail.com>
2022-01-19 20:32:02 +0900

    add service wallets retrieve api

--- servicewallets.go
+++ servicewallets.go
@@ -164,3 +164,167 @@ func (l LBD) RetrieveBalanceOfSpecificTypeOfNonFungiblesServiceWallet(walletAddr
 	}
 	return all, nil
 }
+
+// Retrieve
+
+type WalletInfomation struct {
+	Name          string `json:"name"`
+	WalletAddress string `json:"walletAddress"`
+	CreatedAt     int64  `json:"createdAt"`
+}
+
+func (l *LBD) RetrieveServiceWalletInformation(walletAddress string) (*WalletInfomation, error) {
+	path := fmt.Sprintf("/v1/wallets/%s", walletAddress)
+
+	r := NewGetRequest(path)
+	resp, err := l.Do(r, true)
+	if err != nil {
+		return nil, err
+	}
+	ret := new(WalletInfomation)
+	return ret, json.Unmarshal(resp.ResponseData, ret)
+}
+
+func (l *LBD) RetrieveServiceWalletTransactionHistory(walletAddress string) ([]*Transaction, error) {
+	r := NewGetRequest(fmt.Sprintf("/v1/wallets/%s/transactions", walletAddress))
+
+	data, err := l.Do(r, true)
+	if err != nil {
+		return nil, err
+	}
+
+	ret := []*Transaction{}
+	return ret, json.Unmarshal(data.ResponseData, &ret)
+}
+
+type RetrieveBaseCoinBalance struct {
+	Symbol   string `json:"symbol"`
+	Amount   string `json:"amount"`
+	Decimals int64  `json:"decimals"`
+}
+
+func (l *LBD) RetrieveBaseCoinBalance(walletAddress string) (*RetrieveBaseCoinBalance, error) {
+	path := fmt.Sprintf("/v1/wallets/%s/base-coin", walletAddress)
+
+	r := NewGetRequest(path)
+	resp, err := l.Do(r, true)
+	if err != nil {
+		return nil, err
+	}
+	ret := new(RetrieveBaseCoinBalance)
+	return ret, json.Unmarshal(resp.ResponseData, ret)
+}
+
+type RetrieveBalanceServiceTokensResponce struct {
+	ContractID string `json:"contractId"`
+	Name       string `json:"name"`
+	Symbol     string `json:"symbol"`
+	Amount     string `json:"amount"`
+	Decimals   int64  `json:"decimals"`
+	ImgUri     string `json:"imgUri"`
+}
+
+func (l *LBD) RetrieveBalanceAllServiceTokens(walletAddress string) ([]*RetrieveBalanceServiceTokensResponce, error) {
+	path := fmt.Sprintf("/v1/wallets/%s/service-tokens", walletAddress)
+
+	all := []*RetrieveBalanceServiceTokensResponce{}
+	page := 1
+	for {
+		r := NewGetRequest(path)
+		r.pager.Page = page
+		r.pager.OrderBy = "asc"
+		resp, err := l.Do(r, true)
+		if err != nil {
+			return nil, err
+		}
+		ret := []*RetrieveBalanceServiceTokensResponce{}
+		err = json.Unmarshal(resp.ResponseData, &ret)
+		if err != nil {
+			return nil, err
+		}
+		if len(ret) == 0 {
+			break
+		}
+		all = append(all, ret...)
+		page++
+	}
+
+	return all, nil
+}
+
+func (l *LBD) RetrieveBalanceSpecificServiceTokenWallet(walletAddress, contractId string) (*RetrieveBalanceServiceTokensResponce, error) {
+	path := fmt.Sprintf("/v1/wallets/%s/service-tokens/%s", walletAddress, contractId)
+
+	r := NewGetRequest(path)
+	resp, err := l.Do(r, true)
+	if err != nil {
+		return nil, err
+	}
+	ret := new(RetrieveBalanceServiceTokensResponce)
+	return ret, json.Unmarshal(resp.ResponseData, ret)
+}
+
+type RetrieveBalanceFungibles struct {
+	TokenType string `json:"tokenType"`
+	Name      string `json:"name"`
+	Meta      string `json:"meta"`
+	Amount    string `json:"amount"`
+}
+
+func (l *LBD) RetrieveBalanceAllFungibles(walletAddress, contactId string) ([]*RetrieveBalanceFungibles, error) {
+	path := fmt.Sprintf("/v1/wallets/%s/item-tokens/%s/fungibles", walletAddress, contactId)
+
+	all := []*RetrieveBalanceFungibles{}
+	page := 1
+	for {
+		r := NewGetRequest(path)
+		r.pager.Page = page
+		r.pager.OrderBy = "asc"
+		resp, err := l.Do(r, true)
+		if err != nil {
+			return nil, err
+		}
+		ret := []*RetrieveBalanceFungibles{}
+		err = json.Unmarshal(resp.ResponseData, &ret)
+		if err != nil {
+			return nil, err
+		}
+		if len(ret) == 0 {
+			break
+		}
+		all = append(all, ret...)
+		page++
+	}
+
+	return all, nil
+}
+
+func (l *LBD) RetrieveBalanceSpecificFungible(walletAddress, contractId, tokenType string) (*RetrieveBalanceFungibles, error) {
+	path := fmt.Sprintf("/v1/wallets/%s/item-tokens/%s/fungibles/%s", walletAddress, contractId, tokenType)
+
+	r := NewGetRequest(path)
+	resp, err := l.Do(r, true)
+	if err != nil {
+		return nil, err
+	}
+	ret := new(RetrieveBalanceFungibles)
+	return ret, json.Unmarshal(resp.ResponseData, ret)
+}
+
+type RetrieveBalanceNonFungibles struct {
+	TokenIndex string `json:"tokenIndex"`
+	Name       string `json:"name"`
+	Meta       string `json:"meta"`
+}
+
+func (l *LBD) RetrieveBalanceSpecificNonFungible(walletAddress, contractId, tokenType, tokenIndex string) (*RetrieveBalanceNonFungibles, error) {
+	path := fmt.Sprintf("/v1/wallets/%s/item-tokens/%s/non-fungibles/%s/%s", walletAddress, contractId, tokenType, tokenIndex)
+
+	r := NewGetRequest(path)
+	resp, err := l.Do(r, true)
+	if err != nil {
+		return nil, err
+	}
+	ret := new(RetrieveBalanceNonFungibles)
+	return ret, json.Unmarshal(resp.ResponseData, ret)
+}
--- servicewallets_test.go
+++ servicewallets_test.go
@@ -7,6 +7,8 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+var walletAddress string = "tlink1fr9mpexk5yq3hu6jc0npajfsa0x7tl427fuveq"
+
 func TestListAllServiceWallets(t *testing.T) {
 	ret, err := l.ListAllServiceWallets()
 	assert.Nil(t, err)
@@ -17,6 +19,65 @@ func TestListAllServiceWallets(t *testing.T) {
 func TestTransferBaseCoins(t *testing.T) {
 	onlyTxMode(t)
 	ret, err := l.TransferBaseCoins(owner, toAddress, big.NewInt(1))
+	assert.Nil(t, err)
+	t.Log(ret)
+}
+
+func TestRetrieveServiceWalletInformation(t *testing.T) {
+	onlyTxMode(t)
+	ret, err := l.RetrieveServiceWalletInformation(walletAddress)
+	assert.Nil(t, err)
+	t.Log(ret)
+}
+
+func TestRetrieveServiceWalletTransactionHistory(t *testing.T) {
+	onlyTxMode(t)
+	ret, err := l.RetrieveServiceWalletTransactionHistory(walletAddress)
+	assert.Nil(t, err)
+	t.Log(len(ret))
+	t.Log(*ret[0])
+}
+
+func TestRetrieveBaseCoinBalance(t *testing.T) {
+	onlyTxMode(t)
+	ret, err := l.RetrieveBaseCoinBalance(walletAddress)
+	assert.Nil(t, err)
+	t.Log(ret)
+}
+
+func TestRetrieveBalanceAllServiceTokens(t *testing.T) {
+	onlyTxMode(t)
+	ret, err := l.RetrieveBalanceAllServiceTokens(walletAddress)
+	assert.Nil(t, err)
+	t.Log(len(ret))
+	t.Log(*ret[0])
+}
+
+func TestRetrieveBalanceSpecificServiceTokenWallet(t *testing.T) {
+	onlyTxMode(t)
+	ret, err := l.RetrieveBalanceSpecificServiceTokenWallet(walletAddress, serviceTokenContractId)
+	assert.Nil(t, err)
+	t.Log(ret)
+}
+
+func TestRetrieveBalanceAllFungibles(t *testing.T) {
+	onlyTxMode(t)
+	ret, err := l.RetrieveBalanceAllFungibles(walletAddress, itemTokenContractId)
+	assert.Nil(t, err)
+	t.Log(len(ret))
+	t.Log(*ret[0])
+}
+
+func TestRetrieveBalanceSpecificFungible(t *testing.T) {
+	onlyTxMode(t)
+	ret, err := l.RetrieveBalanceSpecificFungible(walletAddress, itemTokenContractId, tokenType)
+	assert.Nil(t, err)
+	t.Log(ret)
+}
+
+func TestRetrieveBalanceSpecificNonFungible(t *testing.T) {
+	onlyTxMode(t)
+	ret, err := l.RetrieveBalanceSpecificNonFungible(walletAddress, itemTokenContractId, tokenType, "00000001")
 	assert.Nil(t, err)
 	t.Log(ret)
 }
