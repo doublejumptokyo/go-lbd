f9ae0fd5eac7cc1965f5a89936d60ae6706050f3
Nakamura Kazuya <kazuya.nakamura@scopenext.jp>
2022-02-03 08:54:24 +0000

    Add utils

---
+++ utils.go
@@ -0,0 +1,236 @@
+package lbd
+
+type UserHoldersNonFungible struct {
+	userId       string
+	NonFungibles []*NonFungible
+}
+
+func (l LBD) RetrieveUserHoldersNonFungibles(userIds []string, contractId, tokenType string) ([]*UserHoldersNonFungible, error) {
+	all := []*UserHoldersNonFungible{}
+	for _, u := range userIds {
+		uret, err := l.RetrieveBalanceOfAllNonFungiblesUserWallet(u, contractId)
+		if err != nil {
+			return nil, err
+		}
+		d := &UserHoldersNonFungible{
+			userId:       u,
+			NonFungibles: uret,
+		}
+		all = append(all, d)
+	}
+	return all, nil
+}
+
+func (l LBD) ListBalanceOfAllServiceTokensUserWallet(userIds []string) ([]*BalanceOfServiceTokens, error) {
+	all := []*BalanceOfServiceTokens{}
+	for _, u := range userIds {
+		ret, err := l.RetrieveBalanceOfAllServiceTokensUserWallet(u)
+		if err != nil {
+			continue
+		}
+		for _, r := range ret {
+			b := &BalanceOfServiceTokens{
+				ContractId: r.ContractId,
+				Name:       r.Name,
+				Symbol:     r.Symbol,
+				ImgUri:     r.ImgUri,
+				Decimals:   r.Decimals,
+				Amount:     r.Amount,
+			}
+			all = append(all, b)
+		}
+	}
+	return all, nil
+}
+
+type UserHoldersFungible struct {
+	UserId    string
+	Fungibles []*BalanceOfFungible
+}
+
+func (l LBD) RetrieveUserHoldersFungibles(userIds []string, contractId, tokenType string) ([]*UserHoldersFungible, error) {
+	all := []*UserHoldersFungible{}
+	for _, u := range userIds {
+		uret, err := l.RetrieveBalanceOfAllFungiblesUserWallet(u, contractId)
+		if err != nil {
+			continue
+		}
+		d := &UserHoldersFungible{
+			UserId:    u,
+			Fungibles: uret,
+		}
+		all = append(all, d)
+	}
+	return all, nil
+}
+
+type UserNonFungibleTokens struct {
+	UserId           string
+	NonFungibleToken []*NonFungibleToken
+}
+
+func (l LBD) ListBalanceOfSpecificTypeOfNonFungiblesUserWallet(userIds []string, contractId, tokenType string) ([]*UserNonFungibleTokens, error) {
+	all := []*UserNonFungibleTokens{}
+	for _, u := range userIds {
+		ret, err := l.RetrieveBalanceOfSpecificTypeOfNonFungiblesUserWallet(u, contractId, tokenType)
+		if err != nil {
+			continue
+		}
+		n := &UserNonFungibleTokens{
+			UserId:           u,
+			NonFungibleToken: ret,
+		}
+		all = append(all, n)
+	}
+	return all, nil
+}
+
+type UserBalanceOfNonFungiblesTokenType struct {
+	UserId                         string
+	BalanceOfNonFungiblesTokenType *BalanceOfNonFungiblesTokenType
+}
+
+func (l LBD) ListBalanceOfNonFungiblesWithTokenTypeUserWallet(userIds []string, contractId, orderBy, pageToken string, limit uint32) ([]*UserBalanceOfNonFungiblesTokenType, error) {
+	all := []*UserBalanceOfNonFungiblesTokenType{}
+	for _, u := range userIds {
+		ret, err := l.RetrieveBalanceOfNonFungiblesWithTokenTypeUserWallet(u, contractId, orderBy, pageToken, limit)
+		if err != nil {
+			continue
+		}
+		b := &UserBalanceOfNonFungiblesTokenType{
+			UserId:                         u,
+			BalanceOfNonFungiblesTokenType: ret,
+		}
+		all = append(all, b)
+	}
+	return all, nil
+}
+
+type UserTransactionHistory struct {
+	UserId      string
+	Transaction []*Transaction
+}
+
+func (l LBD) ListUserWalletTransactionHistoryUserWallet(userIds []string) ([]*UserTransactionHistory, error) {
+	all := []*UserTransactionHistory{}
+	for _, u := range userIds {
+		ret, err := l.RetrieveUserWalletTransactionHistory(u)
+		if err != nil {
+			continue
+		}
+		t := &UserTransactionHistory{
+			UserId:      u,
+			Transaction: ret,
+		}
+		all = append(all, t)
+	}
+	return all, nil
+}
+
+type WalletAddressFungibleTokens struct {
+	WalletAddress string
+	FungibleToken []*RetrieveBalanceFungibles
+}
+
+func (l LBD) RetrieveWalletAddressBalanceOfAllFungibleServiceWallet(wallets []string, contractId string) ([]*WalletAddressFungibleTokens, error) {
+	all := []*WalletAddressFungibleTokens{}
+	for _, w := range wallets {
+		ret, err := l.RetrieveBalanceAllFungibles(w, contractId)
+		if err != nil {
+			continue
+		}
+		u := &WalletAddressFungibleTokens{
+			WalletAddress: w,
+			FungibleToken: ret,
+		}
+		all = append(all, u)
+	}
+	return all, nil
+}
+
+type WalletAddressNonFungibleTokens struct {
+	WalletAddress    string
+	NonFungibleToken []*NonFungible
+}
+
+func (l LBD) RetrieveWalletAddressBalanceOfAllNonFungiblesServiceWallet(wallets []string, contractId string) ([]*WalletAddressNonFungibleTokens, error) {
+	all := []*WalletAddressNonFungibleTokens{}
+	for _, w := range wallets {
+		ret, err := l.RetrieveBalanceOfAllNonFungiblesServiceWallet(w, contractId)
+		if err != nil {
+			continue
+		}
+		u := &WalletAddressNonFungibleTokens{
+			WalletAddress:    w,
+			NonFungibleToken: ret,
+		}
+		all = append(all, u)
+	}
+	return all, nil
+}
+
+type WalletAddressTransactionHistory struct {
+	WalletAddress string
+	Transaction   []*Transaction
+}
+
+func (l LBD) ListServiceWalletTransactionHistory(wallets []string) ([]*WalletAddressTransactionHistory, error) {
+	all := []*WalletAddressTransactionHistory{}
+	for _, w := range wallets {
+		ret, err := l.RetrieveServiceWalletTransactionHistory(w)
+		if err != nil {
+			continue
+		}
+		t := &WalletAddressTransactionHistory{
+			WalletAddress: w,
+			Transaction:   ret,
+		}
+		all = append(all, t)
+	}
+	return all, nil
+}
+
+type WalletAddressServiceTokens struct {
+	WalletAddress string
+	ServiceTokens []*RetrieveBalanceServiceTokensResponse
+}
+
+func (l LBD) ListWalletAddresseBalanceAllServiceTokens(wallets []string) ([]*WalletAddressServiceTokens, error) {
+	all := []*WalletAddressServiceTokens{}
+	for _, w := range wallets {
+		ret, err := l.RetrieveBalanceAllServiceTokens(w)
+		if err != nil {
+			continue
+		}
+		s := &WalletAddressServiceTokens{
+			WalletAddress: w,
+			ServiceTokens: ret,
+		}
+		all = append(all, s)
+	}
+	return all, nil
+}
+
+func (l LBD) ListNonFungibleTokenType(contractId string, tokenTypes []string) ([]*TokenType, error) {
+	all := []*TokenType{}
+	for _, t := range tokenTypes {
+		ret, err := l.RetrieveNonFungibleTokenType(contractId, t, nil)
+		if err != nil {
+			continue
+		}
+		all = append(all, ret)
+	}
+	return all, nil
+}
+
+func (l LBD) ListFungibleInformation(contractId string, tokenTypes []string) ([]*FungibleInformation, error) {
+	all := []*FungibleInformation{}
+	for _, t := range tokenTypes {
+		ret, err := l.RetrieveFungibleInformation(contractId, t)
+		if err != nil {
+			continue
+		}
+		all = append(all, ret)
+	}
+	return all, nil
+}
---
+++ utils_test.go
@@ -0,0 +1,237 @@
+package lbd
+
+import "testing"
+
+func TestRetrieveUserHoldersNonFungibles(t *testing.T) {
+	ret, err := l.RetrieveUserHoldersNonFungibles([]string{userId}, itemTokenContractId, tokenType)
+	if err != nil {
+		t.Fatal(err)
+	}
+	for _, r := range ret {
+		t.Log(r.userId)
+		for _, m := range r.NonFungibles {
+			t.Logf("Name: %s, Meta: %s, TokenType: %s, NumberOfIndex: %s", m.Name, m.Meta, m.TokenType, m.NumberOfIndex)
+		}
+	}
+}
+
+func TestListBalanceOfAllServiceTokensUserWallet(t *testing.T) {
+	ret, err := l.ListBalanceOfAllServiceTokensUserWallet([]string{userId, "U69b8d49495102d6eb3148f7b37f2ba5a", "U69b8d49495102d6eb3148f7b37f2ba5e"})
+	if err != nil {
+		t.Fatal(err)
+	}
+	for _, r := range ret {
+		t.Logf("Name: %s, ContractId: %s, Decimals: %d, ImgUri: %s, Amount: %s, Symbol: %s", r.Name, r.ContractId, r.Decimals, r.ImgUri, r.Amount, r.Symbol)
+	}
+}
+
+func TestRetrieveUserHoldersFungibles(t *testing.T) {
+	ret, err := l.RetrieveUserHoldersFungibles([]string{userId}, itemTokenContractId, fungibleTokenType)
+	if err != nil {
+		t.Fatal(err)
+	}
+	for _, m := range ret {
+		t.Log(m.UserId)
+		for _, f := range m.Fungibles {
+			t.Logf("Name: %s, Meta: %s, TokenType: %s, Amount: %s",
+				f.Name,
+				f.Meta,
+				f.TokenType,
+				f.Amount,
+			)
+		}
+	}
+}
+
+func TestListBalanceOfSpecificTypeOfNonFungiblesUserWallet(t *testing.T) {
+	ret, err := l.ListBalanceOfSpecificTypeOfNonFungiblesUserWallet([]string{userId}, itemTokenContractId, tokenType)
+	if err != nil {
+		t.Fatal(err)
+	}
+	for _, r := range ret {
+		t.Log(r.UserId)
+		for _, n := range r.NonFungibleToken {
+			t.Logf("Name: %s, Meta: %s, TokenType: %s, TokenIndex: %s",
+				n.Name,
+				n.Meta,
+				n.TokenType,
+				n.TokenIndex,
+			)
+		}
+	}
+}
+
+func TestListBalanceOfNonFungiblesWithTokenTypeUserWallet(t *testing.T) {
+	ret, err := l.ListBalanceOfNonFungiblesWithTokenTypeUserWallet([]string{userId}, itemTokenContractId, "asc", "", 10)
+	if err != nil {
+		t.Fatal(err)
+	}
+	for _, r := range ret {
+		t.Logf("UserId: %s", r.UserId)
+		t.Logf("NextPageToken: %s", r.BalanceOfNonFungiblesTokenType.NextPageToken)
+		t.Logf("PrePageToken: %s", r.BalanceOfNonFungiblesTokenType.PrePageToken)
+		for _, b := range r.BalanceOfNonFungiblesTokenType.List {
+			t.Logf("Token\n Name: %s, TokenId: %s, CreatedAt: %d, BurnedAt: %d",
+				b.Token.Name,
+				b.Token.TokenId,
+				b.Token.CreatedAt,
+				b.Token.BurnedAt,
+			)
+			t.Logf("Type\n Name: %s, Meta: %s TokenType: %s, CreatedAt: %d, TotalBurn: %s, TotalMint: %s, TotalSupply: %s",
+				b.Type.Name,
+				b.Type.Meta,
+				b.Type.TokenType,
+				b.Type.CreatedAt,
+				b.Type.TotalBurn,
+				b.Type.TotalMint,
+				b.Type.TotalSupply,
+			)
+		}
+	}
+}
+
+func TestListUserWalletTransactionHistoryUserWallet(t *testing.T) {
+	ret, err := l.ListUserWalletTransactionHistoryUserWallet([]string{userId})
+	if err != nil {
+		t.Fatal(err)
+	}
+	for _, r := range ret {
+		t.Logf("UserId: %s", r.UserId)
+		printTransactionHistory(t, r.Transaction)
+	}
+}
+
+func TestRetrieveWalletAddressBalanceOfAllNonFungiblesServiceWallet(t *testing.T) {
+	ret, err := l.RetrieveWalletAddressBalanceOfAllNonFungiblesServiceWallet([]string{owner.Address}, itemTokenContractId)
+	if err != nil {
+		t.Fatal(err)
+	}
+	for _, r := range ret {
+		t.Logf("WalletAddress: %s", r.WalletAddress)
+		for _, n := range r.NonFungibleToken {
+			t.Logf("NonFungibleTokens\n Name: %s, Meta: %s, TokenType: %s, NumberOfIndex: %s", n.Name, n.Meta, n.TokenType, n.NumberOfIndex)
+		}
+	}
+}
+
+func TestRetrieveWalletAddressBalanceOfAllFungibleServiceWallet(t *testing.T) {
+	ret, err := l.RetrieveWalletAddressBalanceOfAllFungibleServiceWallet([]string{owner.Address}, itemTokenContractId)
+	if err != nil {
+		t.Fatal(err)
+	}
+	for _, r := range ret {
+		t.Logf("WalletAddress: %s", r.WalletAddress)
+		for _, f := range r.FungibleToken {
+			t.Logf("FungibleTokens\n Name: %s, Meta: %s, TokenType: %s, Amount: %s", f.Name, f.Meta, f.TokenType, f.Amount)
+		}
+	}
+}
+
+func TestListServiceWalletTransactionHistory(t *testing.T) {
+	ret, err := l.ListServiceWalletTransactionHistory([]string{owner.Address})
+	if err != nil {
+		t.Fatal(err)
+	}
+	for _, r := range ret {
+		t.Logf("WalletAddress: %s", r.WalletAddress)
+		printTransactionHistory(t, r.Transaction)
+	}
+}
+
+func TestListWalletAddresseBalanceAllServiceTokens(t *testing.T) {
+	ret, err := l.ListWalletAddresseBalanceAllServiceTokens([]string{owner.Address})
+	if err != nil {
+		t.Fatal(err)
+	}
+	for _, r := range ret {
+		t.Logf("WalletAddress: %s", r.WalletAddress)
+		for _, s := range r.ServiceTokens {
+			t.Logf("ServiceTokens \n Name: %s, ContractId: %s, Amount: %s, Symbol: %s, Decimals: %d, ImgUri: %s",
+				s.Name,
+				s.ContractID,
+				s.Amount,
+				s.Symbol,
+				s.Decimals,
+				s.ImgUri,
+			)
+		}
+	}
+}
+
+func TestListNonFungibleTokenType(t *testing.T) {
+	ret, err := l.ListNonFungibleTokenType(itemTokenContractId, []string{tokenType})
+	if err != nil {
+		t.Fatal(err)
+	}
+	t.Log(ret)
+	for _, r := range ret {
+		t.Logf("TokenTypes \n Name: %s, Meta, %s, Token, %+v, TokenType: %s, TotalBurn: %s, TotalMint: %s, TotalSupply: %s, CreatedAt: %d",
+			r.Name,
+			r.Meta,
+			r.Token,
+			r.TokenType,
+			r.TotalBurn,
+			r.TotalMint,
+			r.TotalSupply,
+			r.CreatedAt,
+		)
+		for _, k := range r.Token {
+			t.Logf("Token \n Name: %s, Meta: %s, TokenIndex: %s, CreatedAt: %d, BurnedAt: %d",
+				k.Name,
+				k.Meta,
+				k.TokenIndex,
+				k.CreatedAt,
+				k.BurnedAt,
+			)
+		}
+	}
+}
+
+func TestListFungibleInformation(t *testing.T) {
+	ret, err := l.ListFungibleInformation(itemTokenContractId, []string{fungibleTokenType, "00000002"})
+	if err != nil {
+		t.Fatal(err)
+	}
+	for _, r := range ret {
+		t.Logf("Token \n Name: %s, Meta: %s, TokenType: %s, TotalBurn: %s, TotalMint: %s, TotalSupply: %s, CreatedAt: %d",
+			r.Name,
+			r.Meta,
+			r.TokenType,
+			r.TotalBurn,
+			r.TotalMint,
+			r.TotalSupply,
+			r.CreatedAt,
+		)
+	}
+}
+
+func printTransactionHistory(t *testing.T, transactions []*Transaction) {
+	for _, a := range transactions {
+		t.Logf("Transaction\n Height: %d, TxHash: %s, CodeSpace: %s, Code: %d, Index: %d, GasUsed: %d, GasWanted: %d, TimeStamp: %d",
+			a.Height,
+			a.Txhash,
+			a.Codespace,
+			a.Code,
+			a.Index,
+			a.GasUsed,
+			a.GasWanted,
+			a.Timestamp,
+		)
+
+		t.Logf("Tx\n Type: %s, Memo: %s, Msg: %s, Gas: %d, Amount: %+v",
+			a.Tx.Type,
+			a.Tx.Value.Memo,
+			a.Tx.Value.Msg,
+			a.Tx.Value.Fee.Gas,
+			a.Tx.Value.Fee.Amount,
+		)
+		for _, ls := range a.Logs {
+			t.Logf("Logs\n Log: %s, Success: %t, MsgIndex: %d, Events: %+v",
+				ls.Log,
+				ls.Success,
+				ls.MsgIndex,
+				ls.Events,
+			)
+		}
+	}
+}
